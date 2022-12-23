fa81f5142a142df1adf0f7d4b30d9783e7311953
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2022-12-23 10:40:55 -0500

    Extends

--- src/ControlBonificacion.java
+++ src/ControlBonificacion.java
@@ -2,13 +2,19 @@ public class ControlBonificacion {
  private double suma;
  public double registrarSalario(Funcionario funcionario) {
   this.suma = funcionario.getBonificacion() + this.suma;
-  System.out.println("Calculo actual: " + this.suma);
+  System.out.println("Calculo actual de " + funcionario.getNombre() + ": " + this.suma);
   return this.suma;
  }
  
- public double registrarSalario(Gerente gerente) {
-  this.suma = gerente.getBonificacion() + this.suma;
-  System.out.println("Calculo actual: " + this.suma);
-  return this.suma;
- }
+// public double registrarSalario(Gerente gerente) {
+//  this.suma = gerente.getBonificacion() + this.suma;
+//  System.out.println("Calculo actual de " + gerente.getNombre() + ": " + this.suma);
+//  return this.suma;
+// }
+//
+// public double registrarSalario(Contador contador) {
+//  this.suma = contador.getBonificacion() + this.suma;
+//  System.out.println("Calculo actual de " + contador.getNombre() + ": " + this.suma);
+//  return this.suma;
+// }
 }
--- src/TestControlBonificacion.java
+++ src/TestControlBonificacion.java
@@ -2,12 +2,19 @@ public class TestControlBonificacion {
  public static void main(String[] args) {
   Funcionario diego = new Funcionario();
   diego.setSalario(2000);
+  diego.setNombre("Diego");
   
   Gerente jimena = new Gerente();
   jimena.setSalario(10000);
+  jimena.setNombre("Jimena");
+  
+  Contador alexis = new Contador();
+  alexis.setSalario(100);
+  alexis.setNombre("Alexis");
   
   ControlBonificacion controlBonificacion = new ControlBonificacion();
   controlBonificacion.registrarSalario(diego);
   controlBonificacion.registrarSalario(jimena);
+  controlBonificacion.registrarSalario(alexis);
  }
 }
