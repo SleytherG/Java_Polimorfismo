1643c0ec902808f321ba8927ec5aa879d93d57f4
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2022-12-23 09:59:38 -0500

    Herencia

--- src/TestGerente.java
+++ src/TestGerente.java
@@ -2,10 +2,12 @@ public class TestGerente {
  public static void main(String[] args) {
 //  Gerente gerente = new Gerente();
 //  gerente.setSalario(5000);
-  Funcionario gerente = new Funcionario();
+  Gerente gerente = new Gerente();
   gerente.setSalario(6000);
+  gerente.setClave("AluraCursosOnline");
   gerente.setTipo(1);
  
   System.out.println(gerente.getBonificacion());
+  System.out.println(gerente.iniciarSesion("AluraCursosOnline"));
  }
 }
