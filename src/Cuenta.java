cd0b2136ff159099c01ad0bd029070905d2cc3db
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2022-12-23 10:52:49 -0500

    Más sobre polimorfismo

---
+++ src/Cuenta.java
@@ -0,0 +1,91 @@
+public class Cuenta {
+ private double saldo;
+ private int agencia;
+ private int numero;
+ private Cliente titular = new Cliente();
+ 
+ private static int total = 0;
+ 
+ 
+ public Cuenta(int agencia, int numero) {
+  if ( agencia <= 0 ) {
+   System.out.println("No se permite 0");
+   this.agencia = 1;
+  } else {
+   this.agencia = agencia;
+  }
+  this.numero = numero;
+  this.total++;
+  System.out.println("Se van creando: " + this.total + " cuentas");
+ }
+ 
+ public void depositar(double valor) {
+  this.saldo = this.saldo + valor;
+ }
+ 
+ public boolean retirar(double valor) {
+  if (this.saldo >= valor) {
+   this.saldo = this.saldo - valor;
+   return true;
+  } else {
+   return false;
+  }
+ }
+ 
+ public boolean transferir(double valor, Cuenta cuentaDestino) {
+  if (this.saldo >= valor) {
+   this.saldo = this.saldo - valor;
+   cuentaDestino.saldo = cuentaDestino.saldo + valor;
+   return true;
+  }
+  return false;
+ }
+ 
+ public double getSaldo() {
+  return this.saldo;
+ }
+ 
+ public void setSaldo(double saldo) {
+  this.saldo = saldo;
+ }
+ 
+ public int getAgencia() {
+  return this.agencia;
+ }
+ 
+ public void setAgencia(int agencia) {
+  if ( agencia > 0) {
+   this.agencia = agencia;
+  } else {
+   System.out.println("No estan permitidos valores negativos");
+  }
+ }
+ 
+ public int getNumero() {
+  return this.numero;
+ }
+ 
+ public void setNumero(int numero) {
+  if ( numero > 0) {
+   this.numero = numero;
+  } else {
+   System.out.println("No estan permitidos valores negativos");
+  }
+ }
+ 
+ public Cliente getTitular() {
+  return titular;
+ }
+ 
+ public void setTitular(Cliente titular) {
+  this.titular = titular;
+ }
+ 
+ public static int getTotal() {
+  return total;
+ }
+ 
+ public static void setTotal(int total) {
+  Cuenta.total = total;
+ }
+}
---
+++ src/CuentaCorriente.java
@@ -0,0 +1,6 @@
+public class CuentaCorriente extends Cuenta {
+ 
+ public CuentaCorriente(int agencia, int numero) {
+  super(agencia, numero);
+ }
+}
